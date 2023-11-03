513b3110fd493b6f60bcbdac8bf260bf6fb98d31
Dirk Fauth <dirk.fauth@gmail.com>
2023-11-03 14:21:39 +0100

    Update postCreate.sh
    Ensure that the install_ecf_jaxrs.sh script is executable

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -6,6 +6,7 @@ yarn
 
 # install the ECF JAX-RS libraries to the local Maven cache
 cd /workspaces/monolith-single_deployment/service-application
+chmod 777 ./install_ecf_jaxrs.sh
 ./install_ecf_jaxrs.sh
 
 # trigger an initial build of the service application to have the Maven dependencies in place for further builds
